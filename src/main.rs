121604cbc9ac18ac2705c8ea1ea9182e1762885a
Jane Xing <zhouxing@uchicago.edu>
2023-03-22 11:08:34 -0700

    working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use tokio_postgres::{NoTls, Error};
+use tokio_postgres::{NoTls, Error, Client, Connection, Socket};
+use std::{thread, time};
+use std::ptr::null;
+use tokio_postgres::tls::NoTlsStream;
 
 // Using Tokio runtime for async behaviour: https://crates.io/crates/tokio
 #[tokio::main]
@@ -17,12 +20,105 @@ async fn main() -> Result<(), Error>{
         }
     });
 
-    // Do the query
-    let users = client.query("SELECT * FROM t", &[]).await?;
+    call_query_no_for_loop(client).await;
+    // ---- output ----
+    // [call_query_no_for_loop] # prepared_stmts:4
+    // name: s2, stmt: PREPARE s2 AS SELECT * FROM t
+    // name: s3, stmt: PREPARE s3 AS SELECT * FROM pg_catalog.pg_prepared_statements
+    // name: s0, stmt: PREPARE s0 AS SELECT * FROM t
+    // name: s1, stmt: PREPARE s1 AS SELECT * FROM t
 
-    // println!("{:#?}", users);
-    let value: i64 = users[0].get(0);
-    println!("{:#?}", value);
+
+    //  call_query_for_loop(client).await;
+    // ---- output ----
+    // [call_query_for_loop] # prepared_stmts:1
+    // name: s2, stmt: PREPARE s2 AS SELECT * FROM pg_catalog.pg_prepared_statements
+
+    // call_exec_no_for_loop(client).await;
+    // ---- output ----
+    //[call_exec_no_for_loop] # prepared_stmts:1
+    // name: s0, stmt: PREPARE s0 AS SELECT * FROM pg_catalog.pg_prepared_statements
+
+    // call_exec_for_loop(client).await;
+    // ---- output ----
+    // [call_exec_for_loop] # prepared_stmts:1
+    // name: s0, stmt: PREPARE s0 AS SELECT * FROM pg_catalog.pg_prepared_statements
+
+
+    Ok(())
+}
+
+async fn call_query_no_for_loop(client: Client) -> Result<(), Error> {
+        let res = client.query("SELECT * FROM t", &[]).await?;
+        let res = client.query("SELECT * FROM t", &[]).await?;
+        let res = client.query("SELECT * FROM t", &[]).await?;
+
+        // Do the query
+        let users = client.query("SELECT * FROM pg_catalog.pg_prepared_statements", &[]).await?;
+        println!("[call_query_no_for_loop] # prepared_stmts:{:#?}", users.len());
+        for user in users.into_iter() {
+            let name: &str = user.get(0);
+            let stmt: &str = user.get(1);
+            match name {
+                _ => println!("name: {name}, stmt: {stmt}", name=name, stmt=stmt),
+            }
+        }
+
+    Ok(())
+}
+
+async fn call_query_for_loop(client: Client) -> Result<(), Error> {
+    for i in 1..3 {
+        let res = client.query("SELECT * FROM t", &[]).await?;
+    }
+
+    // Do the query
+    let users = client.query("SELECT * FROM pg_catalog.pg_prepared_statements", &[]).await?;
+    println!("[call_query_for_loop] # prepared_stmts:{:#?}", users.len());
+    for user in users.into_iter() {
+        let name: &str = user.get(0);
+        let stmt: &str = user.get(1);
+        match name {
+            _ => println!("name: {name}, stmt: {stmt}", name=name, stmt=stmt),
+        }
+    }
+    Ok(())
+}
+
+async fn call_exec_no_for_loop(client: Client) -> Result<(), Error> {
+    let res = client.execute_raw("INSERT INTO t VALUES ($1, pg_sleep(2)), ($2, pg_sleep(1)), ($3, pg_sleep(1))", &[1i64,2i64,3i64]);
+    let res = client.execute_raw("INSERT INTO t VALUES ($1, pg_sleep(2)), ($2, pg_sleep(1)), ($3, pg_sleep(1))", &[1i64,2i64,3i64]);
+    let res = client.execute_raw("INSERT INTO t VALUES ($1, pg_sleep(2)), ($2, pg_sleep(1)), ($3, pg_sleep(1))", &[1i64,2i64,3i64]);
+
+    // Do the query
+    let users = client.query("SELECT * FROM pg_catalog.pg_prepared_statements", &[]).await?;
+    println!("[call_exec_no_for_loop] # prepared_stmts:{:#?}", users.len());
+    for user in users.into_iter() {
+        let name: &str = user.get(0);
+        let stmt: &str = user.get(1);
+        match name {
+            _ => println!("name: {name}, stmt: {stmt}", name=name, stmt=stmt),
+        }
+    }
+
+    Ok(())
+}
+
+async fn call_exec_for_loop(client: Client) -> Result<(), Error> {
+    for i in 1..3 {
+        let res = client.execute_raw("INSERT INTO t VALUES ($1, pg_sleep(2)), ($2, pg_sleep(1)), ($3, pg_sleep(1))", &[1i64,2i64,3i64]);
+    }
+
+    // Do the query
+    let users = client.query("SELECT * FROM pg_catalog.pg_prepared_statements", &[]).await?;
+    println!("[call_exec_for_loop] # prepared_stmts:{:#?}", users.len());
+    for user in users.into_iter() {
+        let name: &str = user.get(0);
+        let stmt: &str = user.get(1);
+        match name {
+            _ => println!("name: {name}, stmt: {stmt}", name=name, stmt=stmt),
+        }
+    }
 
     Ok(())
 }
